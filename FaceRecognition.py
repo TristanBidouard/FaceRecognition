be1935b19e871d6f7cab5b8c52188ceae985c90f
Tristan Bidouard <Tristan@pc57.home>
2018-12-12 13:37:14 +0100

    v2

--- FaceRecognition.py
+++ FaceRecognition.py
@@ -7,6 +7,15 @@ font = cv2.FONT_HERSHEY_SIMPLEX
 def drawFrameRate(img, frameRate):
 	return cv2.putText(img,"FPS: "+str(int(frameRate)),(10,26),font,0.7,(255,0,255),2,cv2.LINE_AA)
 
+def drawFaces(img, faces, eyes):
+	for (xf,yf,wf,hf) in faces:
+		for (xe,ye,we,he) in eyes:
+			if (xe >= xf) and ((xe + we) <= (xf + wf)):
+				if (ye >= yf) and ((ye + he) <= (yf + hf)):
+					cv2.rectangle(img,(xf,yf),(xf+wf,yf+hf),(0,0,255),3)
+					cv2.rectangle(img,(xe,ye),(xe+we,ye+he),(0,255,0),1)
+
+
 freq = cv2.getTickFrequency()
 frameRateCalc = 1
 
@@ -29,11 +38,13 @@ while True:
 	print "Found "+str(len(faces))+" face(s)"
 	print "Found "+str(len(eyes))+" eye(s)"
 
-	for (x,y,w,h) in faces:
+	"""for (x,y,w,h) in faces:
 	    cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
 
 	for (x,y,w,h) in eyes:
-	    cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
+	    cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)"""
+
+	drawFaces(img, faces, eyes)
 
 	drawFrameRate(img, frameRateCalc)
 	cv2.imshow("Face Recognition", img)
